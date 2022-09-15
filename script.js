5f960545947e5b7562f985935375c6fecebe7775
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2022-09-15 14:52:19 -0500

    structure html, css and js

--- script.js
+++ script.js
@@ -1,42 +1,26 @@
-// Function that builds a grid in the "container"
-function createGrid(x) {
-    for (var rows = 0; rows < x; rows++) {
-        for (var columns = 0; columns < x; columns++) {
-            $("#container").append("<div class='grid'></div>");
-        };
-    };
-    $(".grid").width(960/x);
-    $(".grid").height(960/x);
-};
+let clear = document.querySelector('clear')
+clear.addEventListener('click', reset)
 
-// Function that clears the grid
-function clearGrid(){
-    $(".grid").remove();
-};
-
-// Function that prompts the user to select the number of boxes in a new grid
-// the function then also creates that new grid
-function refreshGrid(){
-    var z = prompt("How many boxes per side?");
-    clearGrid();
-    createGrid(z);
-};
-
-// Create a 16x16 grid when the page loads.
-// Creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
-// allows the click of a button to prompt the user to create a new grid
-$(document).ready(function() {
-    createGrid(16);
-
-    $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        });
-
-    $(".newGrid").click(function() {
-        refreshGrid();
-
-        $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        });
+function randomSelection(choices) {
+    const RANDOM_NUMBER = Math.floor(Math.random() * choices.length);
+  
+    return choices[RANDOM_NUMBER];
+}
+  
+const CONTAINER = document.querySelector('.container');
+const COLORS = ['color-red', 'color-blue', 'color-purple', 'color-white'];
+  
+for (let i = 1; i <= 16 * 16; i++) {
+    const SQUARE_DIV = document.createElement('div');
+    SQUARE_DIV.classList.add('color-blue');
+  
+    CONTAINER.appendChild(SQUARE_DIV);
+  
+    SQUARE_DIV.addEventListener('mousedown', () => {
+      SQUARE_DIV.classList.add(randomSelection(COLORS));
     });
-});
+}
+
+function reset() {
+    location.reload()
+}
